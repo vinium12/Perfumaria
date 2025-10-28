71eebf2dc2535df65425b87a9bc8c14be666bef2
vinium12 <125403506+vinium12@users.noreply.github.com>
2025-10-27 21:46:51 -0300

    feat:add clients and cars pages, add table component

--- perfumaria/src/pages/Home/home.jsx
+++ perfumaria/src/pages/Home/home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Table from '../../components/Table/table'
 
 const Dashboard = () => {
     return (
         <div style={{ padding: '20px' }}>
             <h1>Bem-vindo à Dashboard!</h1>
             <p>Aqui virá o conteúdo principal da sua aplicação (Cards, etc.).</p>
+
+            
         </div>
     );
 }
