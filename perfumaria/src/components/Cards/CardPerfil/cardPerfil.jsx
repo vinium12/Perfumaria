fe3abfd1f65aa693dbfd20fc5339f83cbdcf9ec9
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2025-10-28 12:07:03 -0300

    feat(cards): create profile and data, applied in home page

---
+++ perfumaria/src/components/Cards/CardPerfil/cardPerfil.jsx
@@ -0,0 +1,33 @@
+{/* Classe de Criação do Componente CardPerfil */} 
+
+import React from "react";
+
+{/* Import do CSS do Componente */}
+import styles from "./cardPerfil.module.css";
+
+{/* Função de Criação do Card */}
+const CardPerfil = ({ email, ender, tel, regiao, onEdit }) => {
+  return (
+    <div className={styles.card}>
+      <h3 className={styles.titulo}>Bem-vindo !</h3>
+
+      <div className={styles.infoBase}>
+        <p className={styles.subtitulo}>Informações Base:</p>
+        <ul className={styles.lista}>
+          <li><strong>Email:</strong> {email}</li>
+          <li><strong>Endereço:</strong> {ender}</li>
+          <li><strong>Telefone:</strong> {tel}</li>
+          <li><strong>Região:</strong> {regiao}</li>
+        </ul>
+      </div>
+
+      <div className={styles.botoes}>
+        <button className={styles.botaoEditar} onClick={onEdit}>
+          Editar
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CardPerfil;
