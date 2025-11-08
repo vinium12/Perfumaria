560f2edf471210ee0a34bea412beca55c1718f2b
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2025-11-08 13:04:04 -0300

    chore: add comments in root files to clarify code

--- perfumaria/src/App.jsx
+++ perfumaria/src/App.jsx
@@ -1,4 +1,9 @@
+{
+  /* Classe de Definição da Lógica e Estrutura do Site */
+}
+
 import { useState, useEffect } from "react";
+
 import {
   BrowserRouter as Router,
   Routes,
@@ -7,23 +12,80 @@ import {
   useLocation,
   useNavigate,
 } from "react-router-dom";
+
+{
+  /* Import da Biblioteca de Animações ao Ativar uma Rota do Site  */
+}
 import { AnimatePresence, motion } from "framer-motion";
 
+{
+  /* Import do Componente Header(Navbar) */
+}
 import Header from "./components/Header/header";
+
+{
+  /* Import da Página de Login */
+}
 import Login from "./pages/Login/login";
+
+{
+  /* Import da Página de Dashboard */
+}
 import Dashboard from "./pages/Home/home";
+
+{
+  /* Import da Página com as Informações dos Clientes */
+}
 import Clientes from "./pages/Clients/clients";
+
+{
+  /* Import da Página com as Informações da Frota da Perfumaria */
+}
 import Cars from "./pages/Cars/cars";
+
+{
+  /* Import da Página de Edição das Informações do Perfil */
+}
 import EditarPerfil from "./pages/EditPerfil/editPerfil";
+
+{
+  /* Import da Página de Edição dos Produtos Registrados */
+}
 import EditarProduto from "./pages/EditProduct/editproduct";
+
+{
+  /* Import da Página de Cadastro de Produtos */
+}
 import CadastrarProduto from "./pages/CadastrarProduct/cadastrarProduct";
+
+{
+  /* Import da Página de Visualização dos Produtos Cadastrados */
+}
 import Produtos from "./pages/Produtos/produtos";
+
+{
+  /* Import da Página de Edição de uma Venda Realizada */
+}
 import EditarVenda from "./pages/EditarVendas/editarVenda";
+
+{
+  /* Import da Página com o Histórico de Vendas */
+}
 import Vendas from "./pages/Vendas/vendas";
+
+{
+  /* Import da Página de Registro de uma Nova Venda */
+}
 import CadastrarVenda from "./pages/CadastrarVenda/cadastrarVenda";
+
+{
+  /* Import do CSS da Classe Principal do Projeto */
+}
 import "./App.css";
 
-// ---------------------- COMPONENTE AUXILIAR ----------------------
+{
+  /* Função Para Ativação das Rotas(Links) do Site */
+}
 function AppRouter({ isLoggedIn, onLogin, onLogout }) {
   const location = useLocation();
   const navigate = useNavigate();
@@ -57,9 +119,11 @@ function AppRouter({ isLoggedIn, onLogin, onLogout }) {
       />
 
       <main style={{ padding: mainPadding }}>
-        {/* AnimatePresence detecta entrada e saída de rotas */}
+        
+        {/* Método do Motion Framer Para Identificar a Ativação de Rotas */}
         <AnimatePresence mode="wait">
-          {/* motion.div aplica animação a cada troca de página */}
+
+          {/* Método do Motion Framer Para Aplicar a Animação nas Trocas de Rota */}
           <motion.div
             key={location.pathname}
             initial={{ opacity: 0, y: 10 }}
@@ -70,91 +134,118 @@ function AppRouter({ isLoggedIn, onLogin, onLogout }) {
             <Routes location={location} key={location.pathname}>
               <Route path="/login" element={<Login onLogin={onLogin} />} />
 
+              {/* Rota da Página com a Dashboard */}
               <Route
                 path="/"
                 element={
                   <ProtectedRoute isLoggedIn={isLoggedIn}>
+                    {" "}
+                    {/* Aplicando a Função Para Exigir Login */}
                     <Dashboard />
                   </ProtectedRoute>
                 }
               />
 
+              {/* Rota da Página com as Informações do Cliente */}
               <Route
                 path="/clientes"
                 element={
                   <ProtectedRoute isLoggedIn={isLoggedIn}>
+                    {" "}
+                    {/* Aplicando a Função Para Exigir Login */}
                     <Clientes />
                   </ProtectedRoute>
                 }
               />
 
+              {/* Rota da Página com as Informações da Frota */}
               <Route
                 path="/frota"
                 element={
                   <ProtectedRoute isLoggedIn={isLoggedIn}>
+                    {" "}
+                    {/* Aplicando a Função Para Exigir Login */}
                     <Cars />
                   </ProtectedRoute>
                 }
               />
 
+              {/* Rota da Página com as Informações dos Produtos */}
               <Route
                 path="/produtos"
                 element={
                   <ProtectedRoute isLoggedIn={isLoggedIn}>
+                    {" "}
+                    {/* Aplicando a Função Para Exigir Login */}
                     <Produtos />
                   </ProtectedRoute>
                 }
               />
 
+              {/* Rota da Página com as Vendas Realizadas */}
               <Route
                 path="/vendas"
                 element={
                   <ProtectedRoute isLoggedIn={isLoggedIn}>
+                    {" "}
+                    {/* Aplicando a Função Para Exigir Login */}
                     <Vendas />
                   </ProtectedRoute>
                 }
               />
 
+              {/* Rota da Página de Edição dos Dados do Perfil */}
               <Route
                 path="/editar"
                 element={
                   <ProtectedRoute isLoggedIn={isLoggedIn}>
+                    {" "}
+                    {/* Aplicando a Função Para Exigir Login */}
                     <EditarPerfil />
                   </ProtectedRoute>
                 }
               />
 
+              {/* Rota da Página de Edição dos Dados dos Produtos */}
               <Route
                 path="/editarproduto"
                 element={
                   <ProtectedRoute isLoggedIn={isLoggedIn}>
+                    {" "}
+                    {/* Aplicando a Função Para Exigir Login */}
                     <EditarProduto />
                   </ProtectedRoute>
                 }
               />
 
+              {/* Rota da Página de Edição das Vendas Registradas */}
               <Route
                 path="/editarvenda/:id"
                 element={
                   <ProtectedRoute isLoggedIn={isLoggedIn}>
+                    {/* Aplicando a Função Para Exigir Login */}
                     <EditarVenda />
                   </ProtectedRoute>
                 }
               />
 
+              {/* Rota da Página de Cadastro de Produtos */}
               <Route
                 path="/cadastrarproduto"
                 element={
                   <ProtectedRoute isLoggedIn={isLoggedIn}>
+                    {/* Aplicando a Função Para Exigir Login */}
                     <CadastrarProduto />
                   </ProtectedRoute>
                 }
               />
 
+              {/* Rota da Página de Registro de Novas Vendas */}
               <Route
                 path="/cadastrarvenda"
                 element={
                   <ProtectedRoute isLoggedIn={isLoggedIn}>
+                    {/* Aplicando a Função Para Exigir Login */}
                     <CadastrarVenda />
                   </ProtectedRoute>
                 }
@@ -169,13 +260,17 @@ function AppRouter({ isLoggedIn, onLogin, onLogout }) {
   );
 }
 
-// ---------------------- PROTEÇÃO DE ROTAS ----------------------
+{
+  /* Função Para Restringir o Acesso ao Site e Forçar Autenticação */
+}
 const ProtectedRoute = ({ children, isLoggedIn }) => {
   if (!isLoggedIn) return <Navigate to="/login" replace />;
   return children;
 };
 
-// ---------------------- APP PRINCIPAL ----------------------
+{
+  /* Função de Criação da Classe Principal */
+}
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(() => {
     const user = localStorage.getItem("user");
--- perfumaria/src/main.jsx
+++ perfumaria/src/main.jsx
@@ -1,8 +1,16 @@
+{/* Classe do Método de Renderização Principal da Aplicação */}
+
 import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
+
+{/* Import do CSS da Página da Estrutura do Site */}
 import "./index.css";
+
+{/* Import da Página que Agrupa Todos os Elementos do Site */}
 import App from "./App.jsx";
 
+{/* Método de Renderização Segura do Site */}
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
